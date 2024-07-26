9e46bc2910ee9293b470dc68e58eefa83bc00926
anastasiajavakhishvili12 <anastasiajavakhishvili012@gmail.com>
2024-07-26 20:39:50 +0400

    commit

---
+++ level 20/home work/homework.py
@@ -0,0 +1,22 @@
+
+num=int(input("enter yoar number"))
+if num==18:
+    print("you won dinosaur toy")
+elif num==33:
+    print("you won rebbit toy")
+else:
+    print("you lost")
+
+age=int(input("enter yoar age"))
+if age<13:
+    print("you ar kid")
+elif age>13 and age<20:
+    print("you are a teneiger")
+else:
+    print("you are adulte")
+
+for i in range(20): 
+    if i<10:
+        print("helo world")
+    else:
+        print("good bay world")
