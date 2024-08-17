0722aba771dec249fac6b2a3e15b17d5660bb0a5
anastasiajavakhishvili12 <anastasiajavakhishvili012@gmail.com>
2024-08-17 20:13:21 +0400

    commit

---
+++ level 26/home work/homework.py
@@ -0,0 +1,30 @@
+# def sums(number1,number2):
+#     print(number1+number2)
+
+
+# sums(5,6)
+
+
+# def namravli(number1,number2,number3):
+#     print(number1*number2*number3)
+# namravli(2,3,4)
+
+# def greet(name,surname):
+#     print("hello " + name + surname)
+
+# greet("anastasia","javakhishvili")
+
+
+# def sums(number1,number2):
+#     return number1+number2
+
+# x = sums(1,2)
+
+# print(x)
+
+
+def namravli(number1,number2,number3):
+    return number1*number2*number3
+z = namravli(1,2,3)
+
+print(z)
