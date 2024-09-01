c522969a847302ef9e213049268b971594a32e1f
anastasiajavakhishvili12 <anastasiajavakhishvili012@gmail.com>
2024-09-01 19:20:12 +0400

    commit

---
+++ level 32/class work/classwork.py
@@ -0,0 +1,18 @@
+# def manual_sum(listn):
+#     sum = 0
+#     for i in listn:
+#         sum += i
+#     return sum
+
+# print(manual_sum([1,2,3,4,5,6,6,9]))
+
+
+# def manual_min(listn):
+#     min = listn[0]
+#     for i in listn:
+#         if i < min:
+#             min = i
+#     return min
+
+
+# print(manual_min([1,2,3,4,5,5,6,9,0]))
